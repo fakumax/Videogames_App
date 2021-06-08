edfae9ccda72524192df18b5fd198cd1aaa470a5
Facundo Vergara <fakumax@gmail.com>
2021-06-07 23:26:18 -0300

    Landing Page

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import InsertCoin from '../../assets/img/insert-coin.gif'
+import './Landing.scss';
 
 const Landing = () => {
-    return (
-        <div>
-            <h1>lading page</h1>
-        </div>
-    );
+  return (
+    <div className='Landing'>
+      <h1>Welcome to Videogame Finder</h1>
+      <Link to={'/home'}>
+        <button className='InsertCoin'>Go Home</button>
+      </Link>
+      <img className='InsertCoin' src={InsertCoin}></img>
+    </div>
+  );
 };
 
 export default Landing;
