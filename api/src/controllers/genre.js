7216db37c386f245c7da731c0355049e3bb42a57
Facundo Vergara <fakumax@gmail.com>
2021-06-09 00:19:15 -0300

    Platform add

--- api/src/controllers/genre.js
+++ api/src/controllers/genre.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const { Genre, Platform } = require('../db');
 const { GENRES_URL } = require('../constants');
-const {API_KEY} = process.env;
+const { API_KEY } = process.env;
 
 async function getAllGenreAPI(req, res, next) {
   try {
@@ -18,8 +18,7 @@ async function getAllGenreAPI(req, res, next) {
         exclude: ['createdAt', 'updatedAt'],
       },
     });
-    const dbplatform = await funcName();
-    console.log(dbplatform);
+    const dbplatform = await funcPlatform();
     return res.json(dbresult);
   } catch (err) {
     next(err);
@@ -27,70 +26,69 @@ async function getAllGenreAPI(req, res, next) {
 }
 
 // --- Values from platform
-async function funcName() {
-
-//  const { data } = await axios.get( `https://api.rawg.io/api/platforms?key=506cda2ee2aa4c34b7e764ec5f4c1e08`);
-//  const results = await data.results.map((valor) =>
-//   ({
-   
-//     name: valor.name
-//   })
-// );
-//   return results;
-// }
-const platform = await Platform.bulkcreate([
-  { name: 'PC' },
-  { name: 'PlayStation 5' },
-  { name: 'Xbox One' },
-  { name: 'PlayStation 4' },
-  { name: 'Xbox Series S/X' },
-  { name: 'Nintendo Switch' },
-  { name: 'iOS' },
-  { name: 'Android' },
-  { name: 'Nintendo 3DS' },
-  { name: 'Nintendo DS' },
-  { name: 'Nintendo DSi' },
-  { name: 'macOS' },
-  { name: 'Linux' },
-  { name: 'Xbox 360' },
-  { name: 'Xbox' },
-  { name: 'PlayStation 3' },
-  { name: 'PlayStation 2' },
-  { name: 'PlayStation' },
-  { name: 'PS Vita' },
-  { name: 'PSP' },
-  { name: 'Wii U' },
-  { name: 'Wii' },
-  { name: 'GameCube' },
-  { name: 'Nintendo 64' },
-  { name: 'Game Boy Advance' },
-  { name: 'Game Boy Color' },
-  { name: 'Game Boy' },
-  { name: 'SNES' },
-  { name: 'NES' },
-  { name: 'Classic Macintosh' },
-  { name: 'Apple II' },
-  { name: 'Commodore / Amiga' },
-  { name: 'Atari 7800' },
-  { name: 'Atari 5200' },
-  { name: 'Atari 2600' },
-  { name: 'Atari Flashback' },
-  { name: 'Atari 8-bit' },
-  { name: 'Atari ST' },
-  { name: 'Atari Lynx' },
-  { name: 'Atari XEGS' },
-  { name: 'Genesis' },
-  { name: 'SEGA Saturn' },
-  { name: 'SEGA CD' },
-  { name: 'SEGA 32X' },
-  { name: 'SEGA Master System' },
-  { name: 'Dreamcast' },
-  { name: '3DO' },
-  { name: 'Jaguar' },
-  { name: 'Game Gear' },
-  { name: 'Neo Geo' }
-]) 
+async function funcPlatform() {
+  const platform = [
+    { name: 'PC' },
+    { name: 'PlayStation 5' },
+    { name: 'Xbox One' },
+    { name: 'PlayStation 4' },
+    { name: 'Xbox Series S/X' },
+    { name: 'Nintendo Switch' },
+    { name: 'iOS' },
+    { name: 'Android' },
+    { name: 'Nintendo 3DS' },
+    { name: 'Nintendo DS' },
+    { name: 'Nintendo DSi' },
+    { name: 'macOS' },
+    { name: 'Linux' },
+    { name: 'Xbox 360' },
+    { name: 'Xbox' },
+    { name: 'PlayStation 3' },
+    { name: 'PlayStation 2' },
+    { name: 'PlayStation' },
+    { name: 'PS Vita' },
+    { name: 'PSP' },
+    { name: 'Wii U' },
+    { name: 'Wii' },
+    { name: 'GameCube' },
+    { name: 'Nintendo 64' },
+    { name: 'Game Boy Advance' },
+    { name: 'Game Boy Color' },
+    { name: 'Game Boy' },
+    { name: 'SNES' },
+    { name: 'NES' },
+    { name: 'Classic Macintosh' },
+    { name: 'Apple II' },
+    { name: 'Commodore / Amiga' },
+    { name: 'Atari 7800' },
+    { name: 'Atari 5200' },
+    { name: 'Atari 2600' },
+    { name: 'Atari Flashback' },
+    { name: 'Atari 8-bit' },
+    { name: 'Atari ST' },
+    { name: 'Atari Lynx' },
+    { name: 'Atari XEGS' },
+    { name: 'Genesis' },
+    { name: 'SEGA Saturn' },
+    { name: 'SEGA CD' },
+    { name: 'SEGA 32X' },
+    { name: 'SEGA Master System' },
+    { name: 'Dreamcast' },
+    { name: '3DO' },
+    { name: 'Jaguar' },
+    { name: 'Game Gear' },
+    { name: 'Neo Geo' },
+    { name: 'Web' },
+  ];
+  const results = await platform.map((valor) =>
+    Platform.findOrCreate({
+      where: {
+        name: valor.name,
+      },
+    })
+  );
+  return results;
 }
 module.exports = {
-    getAllGenreAPI,
+  getAllGenreAPI,
 };
