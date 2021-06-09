b3c5c254e57d4c680bbe1b294f18c72d3d57abe9
Facundo Vergara <fakumax@gmail.com>
2021-06-08 23:58:39 -0300

    Platform add

--- api/src/controllers/genre.js
+++ api/src/controllers/genre.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const { Genre } = require('../db');
+const { Genre, Platform } = require('../db');
 const { GENRES_URL } = require('../constants');
 const {API_KEY} = process.env;
 
@@ -29,18 +29,68 @@ async function getAllGenreAPI(req, res, next) {
 // --- Values from platform
 async function funcName() {
 
- const { data } = await axios.get( `https://api.rawg.io/api/platforms?key=506cda2ee2aa4c34b7e764ec5f4c1e08`);
- const results = await data.results.map((valor) =>
-  ({
+//  const { data } = await axios.get( `https://api.rawg.io/api/platforms?key=506cda2ee2aa4c34b7e764ec5f4c1e08`);
+//  const results = await data.results.map((valor) =>
+//   ({
    
-    name: valor.name
-  })
-);
-  return results;
+//     name: valor.name
+//   })
+// );
+//   return results;
+// }
+const platform = await Platform.bulkcreate([
+  { name: 'PC' },
+  { name: 'PlayStation 5' },
+  { name: 'Xbox One' },
+  { name: 'PlayStation 4' },
+  { name: 'Xbox Series S/X' },
+  { name: 'Nintendo Switch' },
+  { name: 'iOS' },
+  { name: 'Android' },
+  { name: 'Nintendo 3DS' },
+  { name: 'Nintendo DS' },
+  { name: 'Nintendo DSi' },
+  { name: 'macOS' },
+  { name: 'Linux' },
+  { name: 'Xbox 360' },
+  { name: 'Xbox' },
+  { name: 'PlayStation 3' },
+  { name: 'PlayStation 2' },
+  { name: 'PlayStation' },
+  { name: 'PS Vita' },
+  { name: 'PSP' },
+  { name: 'Wii U' },
+  { name: 'Wii' },
+  { name: 'GameCube' },
+  { name: 'Nintendo 64' },
+  { name: 'Game Boy Advance' },
+  { name: 'Game Boy Color' },
+  { name: 'Game Boy' },
+  { name: 'SNES' },
+  { name: 'NES' },
+  { name: 'Classic Macintosh' },
+  { name: 'Apple II' },
+  { name: 'Commodore / Amiga' },
+  { name: 'Atari 7800' },
+  { name: 'Atari 5200' },
+  { name: 'Atari 2600' },
+  { name: 'Atari Flashback' },
+  { name: 'Atari 8-bit' },
+  { name: 'Atari ST' },
+  { name: 'Atari Lynx' },
+  { name: 'Atari XEGS' },
+  { name: 'Genesis' },
+  { name: 'SEGA Saturn' },
+  { name: 'SEGA CD' },
+  { name: 'SEGA 32X' },
+  { name: 'SEGA Master System' },
+  { name: 'Dreamcast' },
+  { name: '3DO' },
+  { name: 'Jaguar' },
+  { name: 'Game Gear' },
+  { name: 'Neo Geo' }
+]) 
 }
-
-
-
 module.exports = {
     getAllGenreAPI,
 };
