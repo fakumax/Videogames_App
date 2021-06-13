215f9e54000f511fb0edd9e6c80fe8c33534c926
Facundo Vergara <fakumax@gmail.com>
2021-06-13 12:30:23 -0300

    Details Component

--- client/src/components/Detail/Detail.js
+++ client/src/components/Detail/Detail.js
@@ -17,26 +17,30 @@ const Detail = (props) => {
   return (
     <>
       <Back className='link-back' />
-      <div className='details_videogame'>
-        <div className='details_header'>
-          <h1 className='details_name'>{videogame_by_id.name}</h1>
+      {videogame_by_id.id && videogame_by_id ? (
+        <div className='details_videogame'>
+          <div className='details_header'>
+            <h1 className='details_name'>{videogame_by_id.name}</h1>
+          </div>
+          <div className='details_rigth'>
+            <img src={videogame_by_id.img || defaultimg} />
+            <span>Rating: {videogame_by_id.rating}</span>
+            <span>Release: {videogame_by_id.release}</span>
+          </div>
+          <div className='details_left'>
+            <span className='details_genres'>
+              Genre: {videogame_by_id.genres?.map((v) => v.name).join(' - ')}
+            </span>
+            <span className='details_platforms'>
+              Platforms:{' '}
+              {videogame_by_id.platforms?.map((v) => v.name).join(' - ')}
+            </span>
+            <span>Description: {videogame_by_id.description}</span>
+          </div>
         </div>
-        <div className='details_rigth'>
-          <img src={videogame_by_id.img || defaultimg} />
-          <span>Rating: {videogame_by_id.rating}</span>
-          <span>Release: {videogame_by_id.release}</span>
-        </div>
-        <div className='details_left'>
-          <span className='details_genres'>
-            Genre: {videogame_by_id.genres?.map((v) => v.name).join(' - ')}
-          </span>
-          <span className='details_platforms'>
-            Platforms:{' '}
-            {videogame_by_id.platforms?.map((v) => v.name).join(' - ')}
-          </span>
-          <span>Description: {videogame_by_id.description}</span>
-        </div>
-      </div>
+      ) : (
+        <p className='Body_search'>LOADING</p>
+      )}
     </>
   );
 };
